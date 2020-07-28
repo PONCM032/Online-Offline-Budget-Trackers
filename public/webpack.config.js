099fe3926ffdc785607f20629689bf3be92917af
Melanie <melanie-ponce@hotmail.com>
2020-07-27 21:03:11 -0400

    add webpack.config.js and require dependency

---
+++ public/webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    server: "./server.js",
+    index: "index.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Offline-Online Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An application that allows you to trakc your spending.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("icons/icon-192x192.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
+module.exports = config;
