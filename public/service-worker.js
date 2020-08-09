52a4fbdcfb0ebedd683b14d1a8d8eef9ef2b87b3
Melanie <melanie-ponce@hotmail.com>
2020-08-09 08:02:49 -0400

    add favicon for heroku tail error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,4 @@
 const FILES_TO_CACHE = [
-    "/",
     "/index.html",
     "/index.js",
     "/db.js",
@@ -7,6 +6,7 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
+
   ];
   
   
